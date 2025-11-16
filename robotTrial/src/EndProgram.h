be6776fe70bb70e003ba615716ebed0c4cb406b6
Alison Lam <124309486+AlisonLam515@users.noreply.github.com>
2025-11-16 17:35:24 -0500

    full endProgram

--- robotTrial/src/EndProgram.h
+++ robotTrial/src/EndProgram.h
@@ -1,8 +1,8 @@
+#pragma once
 #include "vex.h"
 #include "UserBackup.h"
 #include "Odometry.h"
 #include "Distribution.h"
-#pragma region VEXcode Generated Robot Configuration
 
 using namespace vex;
 
@@ -11,34 +11,76 @@ using namespace vex;
 touchled TouchLED(1);
 colorsensor Colour(11);
 brain Brain;
-
-class EndProgram : public Distribution
-{
+class EndProgram : public Distribution {
 public:
-    // Constructor that passes Odometry reference to the parent (Distribution)
     EndProgram(Odometry &o) : Distribution(o) {}
 
-    void ShutDown()
-    {
-        while (TouchLED.pressing())
-        {
-        };
-        while (!TouchLED.pressing())
-        {
-        };
+    // Waits for a TouchLED press (no break allowed)
+    void waitForTouch() {
+        while (TouchLED.pressing()){};
+        while (!TouchLED.pressing()){};
+    }
 
-        if (!Colour.isNearObject())
-        {
-            Brain.Screen.clearScreen();
-            Brain.Screen.print("No card left in the deck\nNow shutting down\n");
-            wait(2, seconds);
-            Brain.programStop();
+    // Left button = Yes, Right button = No
+    int askYesNo(const char *message) {
+        bool answered = false;
+        int result = 0;
+
+        Brain.Screen.clearScreen();
+        Brain.Screen.print(message);
+        Brain.Screen.newLine();
+        Brain.Screen.print("Left = Yes");
+        Brain.Screen.newLine();
+        Brain.Screen.print("Right = No");
+
+        while (!answered) {
+            if (Brain.buttonLeft.pressing()) {
+                while (Brain.buttonLeft.pressing());
+                result = 1; // yes
+                answered = true;
+            }
+            if (Brain.buttonRight.pressing()) {
+                while (Brain.buttonRight.pressing());
+                result = 0; // no
+                answered = true;
+            }
         }
-        else
-        {
+        return result;
+    }
+
+    // Main shutdown handler
+    bool handleEnd() {
+        Brain.Screen.clearScreen();
+        Brain.Screen.print("Touch LED to finish");
+
+        waitForTouch();
+
+        if (Colour.isNearObject()) {
             Brain.Screen.clearScreen();
-            Brain.Screen.print("%d cards are left in the deck\n", countRemainingCards());
-            wait(2, seconds);
+            Brain.Screen.print("Remaining cards: ");
+            Brain.Screen.print("%d", countRemainingCards());
+
+            int repeat = askYesNo("Repeat process?");
+            if (repeat == 1) {
+                return true;   
+            } else {
+                int shutdown = askYesNo("Shut down robot?");
+                if (shutdown == 1) {
+                    Brain.Screen.clearScreen();
+                    Brain.Screen.print("Shutting down...");
+                    wait(1500, msec);
+                    Brain.programStop();
+                }
+                return false; 
+            }
         }
+
+        Brain.Screen.clearScreen();
+        Brain.Screen.print("No cards left!");
+        Brain.Screen.newLine();
+        Brain.Screen.print("Shutting down...");
+        wait(1500, msec);
+        Brain.programStop();
+        return false;
     }
 };
